a9f2cfefe5a0f62159234da9445280bc000afab4
Richard Drent <richard@orangebay.fr>
2024-03-08 21:37:10 +0100

    .

--- MaxApp/obj/Debug/net7.0/MaxApp.AssemblyInfo.cs
+++ MaxApp/obj/Debug/net7.0/MaxApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaxApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c1c8d8847329c06e4010048e69349a30723cc89")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaxApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaxApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
